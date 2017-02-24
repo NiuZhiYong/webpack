b5ad8aa6e91c1387295d4ade4865ee1199cd9e6f
nzy <niuzy@kingsilk.net>
2017-02-24 16:01:51 +0800

    tijiao

---
+++ img-loder/main.js
@@ -0,0 +1,15 @@
+var img1 = document.createElement("img");
+img1.src = require("./small.png");
+document.body.appendChild(img1);
+
+var img2 = document.createElement("img");
+img2.src = require("./big.png");
+document.body.appendChild(img2);
+
+var img3 = document.createElement("img");
+img3.src = require("./small.png");
+document.body.appendChild(img3);
+
+var img4 = document.createElement("img");
+img4.src = require("./big.png");
+document.body.appendChild(img4);
---
+++ min-pluging/main.js
@@ -0,0 +1,4 @@
+var longVariableName = 'Hello';
+longVariableName += ' World';
+alert(1);
+document.write('<h1>' + longVariableName + '</h1>');
